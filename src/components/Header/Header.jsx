06f74a6074404a52802d01259d08a4378dfe5a51
Shrey Kulkarni <shrey280302@gmail.com>
2024-11-25 23:09:43 +0530

    Added search bar and its icon

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import "./Header.css";
+import headerData from "./Header.json";
 
 const Header = () => {
   return (
-    <div>
-      
+    <div className="header">
+      <div className="row-wrapper">
+        <div className="search-form">
+          <input type="search" name="" placeholder={headerData.search.placeholder}/>
+          <i class={headerData.search.iconClass}></i>
+        </div>
+        <div className="menu">
+          <ul>
+            
+          </ul>
+        </div>
+      </div>
     </div>
   )
 }
