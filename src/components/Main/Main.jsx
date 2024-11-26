384004780587463541acad6fe04760996322858f
Shrey Kulkarni <shrey280302@gmail.com>
2024-11-27 02:26:02 +0530

    Slider component used

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import "./Main.css"
+import "./Main.css";
+import Slider from './Slider/Slider.jsx';
 
 const Main = () => {
   return (
     <div>
-      
+      <Slider />
     </div>
   )
 }
