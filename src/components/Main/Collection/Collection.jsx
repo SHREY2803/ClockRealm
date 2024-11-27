b451942cc4648a53844f3dccd235cea662fc49a7
Shrey Kulkarni <shrey280302@gmail.com>
2024-11-27 13:05:29 +0530

    Collection comp added.

---
+++ src/components/Main/Collection/Collection.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Collection.css'
+
+const Collection = () => {
+  return (
+    <div>
+      Collection
+    </div>
+  )
+}
+
+export default Collection
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -2,12 +2,15 @@ import React from 'react';
 import "./Main.css";
 import Slider from './Slider/Slider.jsx';
 import Category from './Category/Category.jsx';
+import Collection from './Collection/Collection.jsx';
+
 
 const Main = () => {
   return (
     <div>
       <Slider />
       <Category />
+      <Collection />
     </div>
   )
 }
