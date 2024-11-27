94b135e16d9688349cbe6055a4f754413dab4097
Shrey Kulkarni <shrey280302@gmail.com>
2024-11-27 23:48:51 +0530

    Banner comp updated

--- src/components/Main/Banner/Banner.jsx
+++ src/components/Main/Banner/Banner.jsx
@@ -1,14 +1,36 @@
-import React from 'react'
+import React from 'react';
+import './Banner.css'
+import bannerData from "./Banner.json";
+import BannerImage1 from "../../../assets/banner1.webp";
+import BannerImage2 from "../../../assets/banner2.webp";
+import BannerImage3 from "../../../assets/banner3.webp";
+
+const imageMap = {
+  "banner1.webp": BannerImage1,
+  "banner2.webp": BannerImage2,
+  "banner3.webp": BannerImage3,
+};
 
 const Banner = () => {
   return (
     <div className='banner'>
-        <div className='row-wrapper'>
-            
-        </div>
-      
+      <div className='row-wrapper'>
+        {bannerData.map((item) => (
+          <div
+            className="box"
+            key={item.id}
+            style={{ backgroundImage: `url(${imageMap[item.background]})` }}
+          >
+            <div className="content">
+              <h2>{item.title}</h2>
+              <p>{item.description}</p>
+              <a href={item.link}>Shop Now</a>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Banner
+export default Banner;
