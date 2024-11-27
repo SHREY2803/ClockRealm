e3183bc8221b2162e1aa1d9d303890284ad0a734
Shrey Kulkarni <shrey280302@gmail.com>
2024-11-27 12:47:57 +0530

    Cateogory component added

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import "./Main.css";
 import Slider from './Slider/Slider.jsx';
+import Category from './Category/Category.jsx';
 
 const Main = () => {
   return (
     <div>
       <Slider />
+      <Category />
     </div>
   )
 }
